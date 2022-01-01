1ad419b053331cd77b411da4604e7f22a79130ca
Dmitry Shurov <d.l.shurov@gmail.com>
2022-01-01 22:55:03 +0300

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,7 @@ import pytest
 from .helpers import clear_all_tables_in_database
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(autouse=True)
 def clear_database():
     clear_all_tables_in_database()
     yield
-    # clear_all_tables_in_database()
