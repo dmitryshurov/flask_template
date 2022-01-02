c0bd8f430f9acab8977e52c424149303710a9887
Dmitry Shurov <d.l.shurov@gmail.com>
2022-01-02 23:17:38 +0300

    Refactoring

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,3 @@
-docker run -e TESTS_MASK=$1 --env-file .env --network=flask_template_default --rm -it $(docker build -q -f build/tests/Dockerfile .)
+project_name=${PWD##*/}
+
+docker run -e TESTS_MASK=$1 --env-file .env --network=${project_name}_default --rm -it $(docker build -q -f build/tests/Dockerfile .)
