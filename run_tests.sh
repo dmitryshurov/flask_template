31ed016a6926228db884df57f549bea30843be3b
Dmitry Shurov <d.l.shurov@gmail.com>
2022-01-03 12:55:52 +0300

    Chenged folder structure

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 project_name=${PWD##*/}
 
-docker run -e TESTS_MASK=$1 --env-file .env --network=${project_name}_default --rm -it $(docker build -q -f build/tests/Dockerfile .)
+docker run -e TESTS_MASK=$1 --env-file .env --network=${project_name}_default --rm -it $(docker build -q -f build/services/tests/Dockerfile .)
